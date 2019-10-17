5dc63f1ca3300eccaf9e153738ceb80927f07e04
jean <huangjun1@qk365.com>
2019-10-17 11:22:30 +0800

    去掉mapper红线

--- src/main/java/com/qk365/ocr/mapper/OcrOpLogMapper.java
+++ src/main/java/com/qk365/ocr/mapper/OcrOpLogMapper.java
@@ -2,6 +2,8 @@ package com.qk365.ocr.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qk365.ocr.model.bean.OcrOpLog;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OcrOpLogMapper extends BaseMapper<OcrOpLog> {
 }
--- src/main/java/com/qk365/ocr/mapper/OcrTemplateMapper.java
+++ src/main/java/com/qk365/ocr/mapper/OcrTemplateMapper.java
@@ -2,9 +2,11 @@ package com.qk365.ocr.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qk365.ocr.model.bean.OcrTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OcrTemplateMapper extends BaseMapper<OcrTemplate> {
     List<OcrTemplate> getAllOcrTemplates();
 }
